940f4317e77cadac37bfbd4c71b4433fb398cbe2
Joshua Lobo <joshua.jude.lobo@gmail.com>
2018-12-13 17:48:12 +0000

    some cleanup of comments

--- zoc_mc2_submit.py
+++ zoc_mc2_submit.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+
+
 # dlstbx.go
 #   Process a datacollection
 #
@@ -19,6 +20,10 @@ def lazy_pprint(*args, **kwargs):
   from pprint import pprint
   pprint(*args, **kwargs)
 
+def send_current_dir_to_bash():
+  print (os.getcwd()) #print is important to set the bash variable
+  return os.getcwd()
+
 if __name__ == '__main__':
 
   default_configuration = '/dls_sw/apps/zocalo/secrets/credentials-live.cfg'
@@ -28,30 +33,48 @@ if __name__ == '__main__':
   except workflows.Error as e:
     print("Error: %s\n" % str(e))
 
-  #StompTransport.add_command_line_options(parser)
-  #(options, args) = parser.parse_args(sys.argv[1:])
+
   stomp = StompTransport()
 
   message = { 'recipes': [],
               'parameters': {},
             }
   # Build a custom recipe 
+
+  #remove trailing whitespaces causes isssues in -Gpu
+  #params_without_spaces = list(str(i).rstrip() for i in sys.argv[1:])
+
+
   recipe = {}
   recipe['1'] = {}
   recipe['1']['service'] = "MotionCor2_runner"
   recipe['1']['queue'] = "MotionCor2_runner"
   recipe['1']['parameters'] = {}
-  recipe['1']['parameters']['arguments'] = sys.argv[1:]
+  recipe['1']['parameters']['arguments'] = sys.argv[1:] #params_without_spaces
   recipe['1']['parameters']['cwd'] = os.getcwd()
   recipe['start'] = [[1, []]]
+  #message['custom_recipe'] = recipe
+  #recipe['1']['output']= 2
+
+  #try thumbnaling through zocalo
+  # recipe['2'] = {}
+  # recipe['2']['service'] = "scipion_thumbnail_runner"
+  # recipe['2']['queue'] = "scipion_thumnbnail_runner"
+  # recipe['2']['parameters']=sys.argv[1:]
+
+
+
   message['custom_recipe'] = recipe
 
-  lazy_pprint(message)
+
+
+  send_current_dir_to_bash()
+
 
   stomp.connect()
 
   
-
+  #make a valid zocalo recipe by passing the dict 
   test_valid_recipe = workflows.recipe.Recipe(recipe)
   test_valid_recipe.validate()
 
@@ -62,5 +85,5 @@ if __name__ == '__main__':
     message
   )
 
-  print("\nMotioncor2 job submitted")
+
 
