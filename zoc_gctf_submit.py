1e0c35e1cb3b2d399ac3b433b9448939ebe0246a
Joshua Lobo <joshua.jude.lobo@gmail.com>
2018-12-17 17:02:57 +0000

    working gctf recipie send with pconesa

--- zoc_gctf_submit.py
+++ zoc_gctf_submit.py
@@ -14,42 +14,41 @@ from optparse import SUPPRESS_HELP, OptionParser
 import workflows
 import workflows.recipe
 from workflows.transport.stomp_transport import StompTransport
+import uuid
 
-#/dls/tmp/gda2/dls/m02/data/2018/em12345-01/processed/em12345-01_20181107_1600/Runs/000122_ProtGctf/tmp/GridSquare_401K_Data_FoilHole_5419591_Data_5417705_5417706_20180306_2106-140228_aligned_mic.mrc/ctfEstimation.txt
-#Runs/000122_ProtGctf/extra/GridSquare_401K_Data_FoilHole_5419591_Data_5417705_5417706_20180306_2106-140228_aligned_mic/ctfEstimation.txt
+
+# /dls/tmp/gda2/dls/m02/data/2018/em12345-01/processed/em12345-01_20181107_1600/Runs/000122_ProtGctf/tmp/GridSquare_401K_Data_FoilHole_5419591_Data_5417705_5417706_20180306_2106-140228_aligned_mic.mrc/ctfEstimation.txt
+# Runs/000122_ProtGctf/extra/GridSquare_401K_Data_FoilHole_5419591_Data_5417705_5417706_20180306_2106-140228_aligned_mic/ctfEstimation.txt
 
 def get_output_file(input_file):
-  # output_file = os.path.join(os.getcwd(),
-  #                            os.path.dirname(input_file),
-  #                            os.path.splitext(os.path.basename(input_file)),
-  #
-  #                             'ctfEstimation.txt')
+    # output_file = os.path.join(os.getcwd(),
+    #                            os.path.dirname(input_file),
+    #                            os.path.splitext(os.path.basename(input_file)),
+    #
+    #                             'ctfEstimation.txt')
 
-
-
-  tmp1 = str(os.path.join(os.getcwd(),
+    tmp1 = str(os.path.join(os.getcwd(),
                             os.path.dirname(input_file),
                             os.path.splitext(os.path.basename(input_file))[0],
                             'ctfEstimation.txt'))
 
+    corr_tmp1 = re.sub('(\d+.)ProtGctf/tmp', r'\1ProtGctf/extra', tmp1)
 
-  corr_tmp1 = re.sub('(\d+.)ProtGctf/tmp',r'\1ProtGctf/extra',tmp1)
-  
-  #print ("INPUT is %s" %str(input_file))
-  #print("THIS IS %s" %corr_tmp1)
+    # print ("INPUT is %s" %str(input_file))
+    # print("THIS IS %s" %corr_tmp1)
 
+    output_file = os.path.join(os.getcwd(),
+                               os.path.dirname(input_file),
+                               os.path.splitext(os.path.basename(input_file))[0],
+                               'ctfEstimation.txt')
+    # print(output_file)
 
-  output_file = os.path.join(os.getcwd(),
-                            os.path.dirname(input_file),
-                            os.path.splitext(os.path.basename(input_file))[0],
-                            'ctfEstimation.txt')
-  #print(output_file)
+    return corr_tmp1
 
-  return corr_tmp1
 
 def lazy_pprint(*args, **kwargs):
-  from pprint import pprint
-  #pprint(*args, **kwargs)
+    from pprint import pprint
+    # pprint(*args, **kwargs)
 
 
 # def get_output_file(file_path):
@@ -58,51 +57,57 @@ def lazy_pprint(*args, **kwargs):
 
 if __name__ == '__main__':
 
-  default_configuration = '/dls_sw/apps/zocalo/secrets/credentials-live.cfg'
-  # override default stomp host
-  try:
-    StompTransport.load_configuration_file(default_configuration)
-  except workflows.Error as e:
-    print("Error: %s\n" % str(e))
+    default_configuration = '/dls_sw/apps/zocalo/secrets/credentials-live.cfg'
+    # override default stomp host
+    try:
+        StompTransport.load_configuration_file(default_configuration)
+    except workflows.Error as e:
+        print("Error: %s\n" % str(e))
 
-  #StompTransport.add_command_line_options(parser)
-  #(options, args) = parser.parse_args(sys.argv[1:])
-  stomp = StompTransport()
+    # StompTransport.add_command_line_options(parser)
+    # (options, args) = parser.parse_args(sys.argv[1:])
+    stomp = StompTransport()
 
-  output_file = get_output_file(sys.argv[-1])
+    output_file = get_output_file(sys.argv[-1])
 
-  message = { 'recipes': [],
-              'parameters': {},
-            }
-  # Build a custom recipe 
-  recipe = {}
-  recipe['1'] = {}
-  recipe['1']['service'] = "Gctf_runner"
-  recipe['1']['queue'] = "Gctf_runner"
-  recipe['1']['parameters'] = {}
-  recipe['1']['parameters']['arguments'] = sys.argv[1:] + ['>', output_file]
-  recipe['1']['parameters']['cwd'] = os.getcwd()
-  recipe['start'] = [[1, []]]
-  message['custom_recipe'] = recipe
+    message = {'recipes': [],
+               'parameters': {},
+               }
+    # Build a custom recipe
+    recipe = {}
+    recipe['1'] = {}
+    recipe['1']['service'] = "Gctf_runner"
+    recipe['1']['queue'] = "Gctf_runner"
+    recipe['1']['parameters'] = {}
+    recipe['1']['parameters']['arguments'] = sys.argv[1:] + ['>', output_file]
+    recipe['1']['parameters']['cwd'] = os.getcwd()
 
-#  lazy_pprint(message)
+    message['custom_recipe'] = recipe
 
+    # reply_to = 'transient.gctf.%s' % str(uuid.uuid4())
+    # recipe['1']['output'] = 2
+    # recipe['2'] = {}
+    # recipe['2']['service'] = "relion_refine_call_back"
+    # recipe['2']['queue'] = reply_to
+    # recipe['2']['parameters'] = {}
+    # recipe['2']['output'] = 3
+    # recipe['3'] = {}
 
-  stomp.connect()
+    recipe['start'] = [[1, []]]
 
-  test_valid_recipe = workflows.recipe.Recipe(recipe)
-  test_valid_recipe.validate()
+    stomp.connect()
 
-  stomp.send(
-    'processing_recipe',
-    message
-  )
+    test_valid_recipe = workflows.recipe.Recipe(recipe)
+    test_valid_recipe.validate()
 
-  # So that BASH can pick this up
-  
-  #get_output_file()
-  print(output_file)
+    stomp.send(
+        'processing_recipe',
+        message
+    )
 
+    # So that BASH can pick this up
 
-#print("\nMotioncor2 job submitted")
+    # get_output_file()
+    print(output_file)
 
+# print("\nMotioncor2 job submitted")
