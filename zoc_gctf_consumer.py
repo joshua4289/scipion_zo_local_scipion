4f1ca4def8e4ebd74cb11718c2d91869e5aac6ef
Joshua Lobo <joshua.jude.lobo@gmail.com>
2018-12-17 17:04:25 +0000

    working gctf consumer with pconesa no t queue

--- zoc_gctf_consumer.py
+++ zoc_gctf_consumer.py
@@ -30,51 +30,60 @@ class GctfRunner(CommonService):
 
         self.log.info("Start running Gctf Zocalo")
 
-        import subprocess
         from subprocess import Popen
 
         # get the parameters
-
         session = rw.recipe_step['parameters']
+
         self.log.info(session)
         arguments = session['arguments']
         scipion_dir = session['cwd']
 
         self.log.info("Scipion Dir is %s" % scipion_dir)
-
-        # modify the GPU flag to be the correct GPU for this consumer
-        gpu_index = arguments.index('--gid') + 1
-        arguments[gpu_index] = os.getenv('SGE_HGR_gpu', 'GPU0')[3]
-
         self.log.info("Arguments are '%s'" % ' '.join(arguments))
 
+        # Compose the command and the environment
         cmd = ('source /etc/profile.d/modules.sh;'
                'module load EM/Gctf/1.06;'
                'Gctf '
                ) 
-
         cmd += ' '.join(arguments)
 
         self.log.info(cmd)
-   
+
+        # Run the command
+        from subprocess import PIPE
         p1 = Popen(cmd ,cwd=scipion_dir,shell=True)
-                                           #+ ' '.join(arguments), cwd=scipion_dir,shell=True)
-        out_project_cmd, err_project_cmd = p1.communicate()
+
+        #out_project_cmd, err_project_cmd = p1.communicate()
+        #print(out_project_cmd)
+
+        # Read output and feedback ...but GCTF does not do this
+        # while p1.poll() is None: #True or p1.returncode != 0
+        #     print(p1.stdout.readline())
+        #
+        # t_queue = (rw.recipe[rw.recipe_pointer + 1]['queue'])
 
         p1.wait()
         print ("SCIPION WORK DIR IS  %s" %(scipion_dir))
         print("THESE ARE THE ERROR MESSAGES")
-        print(err_project_cmd)
+        #print(err_project_cmd)
 
 
-        p2 = Popen('touch done.tmp' ,cwd=scipion_dir,shell=True)
-        p2.wait()
+        # p2 = Popen('touch done.tmp' ,cwd=scipion_dir,shell=True)
+        # p2.wait()
 
         self.log.info("Finish running Gctf Zocalo")
+        print(header)
         rw.transport.ack(header)
-        rw.send([])
+        #rw.send([])
 
-    
+            # queue_name = str(t.gctf_runner.
+            # self.log.info("queue that is being listended to is %s" % queue_name)
+            # workflows.recipe.wrap_subscribe(self._transport, queue_name,
+            #                                 self.run_Gctf, acknowledgement=True, log_extender=self.extend_log,
+            #                                 allow_non_recipe_messages=True)
+
     def shutdown_consumer(self):
         ''' Shutdown Consumer based on the timeout mentioned in the Import step of workflow '''
 
